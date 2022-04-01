8871287c1df5b6aae49f715df4546b9b32de0561
KodokuOdius <alanfoxincoolctranica@gmail.com>
2022-04-02 01:14:43 +0300

    is alive?

--- life.py
+++ life.py
@@ -1,6 +1,26 @@
 # Source:
 # https://dementiy.github.io/assignments/life/
 
+# Rules
+# Место действия этой игры — «вселенная» — это размеченная на клетки поверхность или плоскость 
+# — безграничная, ограниченная, или замкнутая (в пределе — бесконечная плоскость).
+
+# Каждая клетка на этой поверхности может находиться в двух состояниях: 
+# быть «живой» (заполненной) или быть «мёртвой» (пустой). Клетка имеет восемь соседей, окружающих её.
+
+# Распределение живых клеток в начале игры называется первым поколением. 
+# Каждое следующее поколение рассчитывается на основе предыдущего по таким правилам:
+#   в пустой (мёртвой) клетке, рядом с которой ровно три живые клетки, зарождается жизнь;
+#   если у живой клетки есть две или три живые соседки, то эта клетка продолжает жить; 
+#   в противном случае, если соседей меньше двух или больше трёх, клетка умирает («от одиночества» или «от перенаселённости»)
+
+# Игра прекращается, если
+# на поле не останется ни одной «живой» клетки
+# конфигурация на очередном шаге в точности (без сдвигов и поворотов) 
+# повторит себя же на одном из более ранних шагов (складывается периодическая конфигурация)
+# при очередном шаге ни одна из клеток не меняет своего состояния 
+# (складывается стабильная конфигурация; предыдущее правило, вырожденное до одного шага назад)
+
 import pygame
 from pygame.locals import *
 
@@ -60,6 +80,8 @@ class LIFE():
 class Universe():
 
     def __init__(self, width, height, speed) -> None:
+        self.width = width
+        self.height = height
         self.speed = speed
 
         self.window = []
@@ -71,18 +93,74 @@ class Universe():
             self.window.append(_temp)
 
 
+    def first_generate(self):
+        from random import sample as s
+
+        first_cells = self.width * self.height * 0.6
+
+        popl = [
+            (i, j) 
+            for i in range(self.height) 
+                for j in range(self.width)
+        ]
+
+        for x, y in s(popl, int(first_cells)):
+            self.window[x][y] = 1
+
+
+
     def show(self):
-        for row in self.window:
-                print(*row)
+        for i in range(self.height):
+            for j in range(self.width):
+                el = self.window[i][j]
+                print(
+                    '\x1b[6;30;42m' + str(el) + '\x1b[0m' if el == 1 
+                    else '\x1b[6;30;41m' + str(el) + '\x1b[0m', 
+                    end=''
+                )
+            print()
+
+
+    def analys(self):   
+        new = self.window
+
+        for i in range(self.height):
+            for j in range(self.width):
+                total_sum = 0
+                if i-1 >= 0:
+                    total_sum += sum(self.window[i-1][j-1 if j-1 >= 0 else j: j+2])
+        
+                total_sum += sum(self.window[i][j-1 if j-1 >= 0 else j : j+2]) - self.window[i][j]
+
+                if i+1 < len(self.window):
+                    total_sum += sum(self.window[i+1][j-1 if j-1 >= 0 else j : j+2])
+                
+                if total_sum == 3 and new[i][j] == 0:
+                    new[i][j] = 1
+                elif total_sum == 2 and new[i][j] == 1:
+                    continue
+                else:
+                    new[i][j] = 0
+
+                #new[i][j] = 1 if total_sum in [2, 3] else 0
+
+        self.window = new
+        
+
 
 
     def start(self, *args, **kwds) -> None:
+        self.first_generate()
         run = True
 
         while run:
             self.show()
-
             time.sleep(1 / self.speed)
+
+            self.analys()
+
+            # input()
+
             os.system("cls")
 
 
@@ -91,9 +169,9 @@ if __name__ == "__main__":
     # game.run()
     print()
 
-    FPS = 60
+    FPS = 120
 
-    life = Universe(20, 15, FPS)
+    life = Universe(55, 15, FPS)
 
     life.start()
 
